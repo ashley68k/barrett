31dd115cd09a2e8c5fb530243e1a67bee9b9b4f1
erysdren <contact@erysdren.me>
2025-10-28 19:46:46 -0500

    replace point_t with SDL_Point

--- src/rt_map.c
+++ src/rt_map.c
@@ -58,7 +58,7 @@ static int oldw, oldh;
 static byte* skytile;
 static int mapcolor = 8;
 
-static point_t arrows[8][7] = {
+static SDL_Point arrows[8][7] = {
 	{{4, 2}, {2, 4}, {2, 3}, {0, 3}, {0, 1}, {2, 1}, {2, 0}},
 	{{4, 0}, {4, 3}, {3, 2}, {1, 4}, {0, 3}, {2, 1}, {1, 0}},
 	{{2, 0}, {4, 2}, {3, 2}, {3, 4}, {1, 4}, {1, 2}, {0, 2}},
